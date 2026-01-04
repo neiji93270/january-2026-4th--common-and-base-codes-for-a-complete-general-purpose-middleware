0ae0d0df3d372f9341fe2cbf2436ee3d1812e3f7
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2026-01-04 11:47:14 +0100

    Update SimilarOttoSecondsBestMatchingDivideConquer.h

--- SimilarOttoSecondsBestMatchingDivideConquer.h
+++ SimilarOttoSecondsBestMatchingDivideConquer.h
@@ -1,3 +1,9 @@
 //to replace wrong signals between other middlewares, we need a range of values, lets say 5250 cipher values in a gigascale table
 //then now we are able to use cpp public methods of contracts inside a whole cpu messages if the deeply scanning processes running inside 
 ///os kernels like linux can permit large seconds timelapses. So we don't need signals. This is then, in this project, a marsahlling with some assertions !
+
+
+
+
+//engines tools suites re the most communicating programs with oother engines. So we cannot have a short line of code tools in a communication port on os s.
+//then, we can inject private keys into signals that are handled by zombi tools suites programs. So we are checking complexity !
