0762d9b0b4e9595fc047738564011ae48ec0e958
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2026-01-04 11:44:15 +0100

    Create SimilarOttoSecondsBestMatchingDivideConquer.h

---
+++ SimilarOttoSecondsBestMatchingDivideConquer.h
@@ -0,0 +1,3 @@
+//to replace wrong signals between other middlewares, we need a range of values, lets say 5250 cipher values in a gigascale table
+//then now we are able to use cpp public methods of contracts inside a whole cpu messages if the deeply scanning processes running inside 
+///os kernels like linux can permit large seconds timelapses. So we don't need signals. This is then, in this project, a marsahlling with some assertions !
